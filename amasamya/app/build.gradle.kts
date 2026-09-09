719cd5992e80af85e099569aa57e5ee241544827
Akhilesh <akhilesh.malani@gmail.com>
2026-09-09 19:00:28 +0530

    bump: Version 1.2 (Build 17) following custom version sequence rule

--- amasamya/app/build.gradle.kts
+++ amasamya/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 17
-        versionName = "1.0.2"
+        versionName = "1.2"
     }
 
     val localProperties = Properties()
